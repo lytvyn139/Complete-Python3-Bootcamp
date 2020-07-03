794721b307ebf3afab0745f9d15787c66d72c65d
IURII <lytvyn.139@gmail.com>
2020-07-03 10:10:10 -0500

    ternary

---
+++ 12-ternary.py
@@ -0,0 +1,10 @@
+#<result_if_true> if <condition> else <result_if_false>
+stacks = 3
+if stacks >= 3:
+    print('Coding Dojo')
+else:
+    print('You are not Coding Dojo!')
+
+print('Coding Dojo' if stacks >= 3 else 'You are not Coding Dojo!')
+
+
