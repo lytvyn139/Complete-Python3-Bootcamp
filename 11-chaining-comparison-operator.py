000bf0d34f1082c406bd82902fede4dfcdae2270
IURII <lytvyn.139@gmail.com>
2020-07-03 10:09:29 -0500

    chaining

---
+++ 11-chaining-comparison-operator.py
@@ -0,0 +1,12 @@
+1 < 2 < 3 #true
+1 < 2 > 3 #false
+#works but not practical
+
+1 < 2 and 2 > 3 #false
+'h' == 'h' and 2 == 2 #true
+('h' == 'h') and (2 == 2) #true
+
+1 == 1 or 2 == 2 #true
+#for true need only 1 side true
+
+not (1 == 1) #false
